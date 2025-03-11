72c3ce58934fab62a3c0827de4c9b48cfde33707
Dmitrii Alekseev <dmitry.alekseev@flatstack.com>
2025-03-12 00:14:22 +0300

    fix-path

---
+++ src/pages/repos/RepoTable.tsx
@@ -0,0 +1,86 @@
+import { useEffect } from "react";
+import { Table, Space } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  fetchRepositories,
+} from "../../store/repos";
+import type { RootState } from "../../store/store";
+
+import { UpdateRepo } from "./UpdateRepo";
+import { DeleteRepoButton } from "./DeleteRepo";
+import { RepoDetailsModal } from "./RepoDescription";
+interface RepoTableProps {
+  login?: string;
+  token?: string | null;
+}
+
+export function RepoTable({ login, token }: RepoTableProps) {
+  const dispatch = useDispatch();
+  const { repositories, status, error } = useSelector(
+    (state: RootState) => state.repos
+  );
+
+  useEffect(() => {
+    if (!login || !token) return;
+    dispatch(fetchRepositories({ login, token }) as any);
+  }, [dispatch, login, token]);
+
+  const columns = [
+    {
+      title: "Название",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Описание",
+      dataIndex: "description",
+      key: "description",
+      render: (text: string | null) => text || "Нет описания",
+    },
+    {
+      title: "Видимость",
+      dataIndex: "visibility",
+      key: "visibility",
+      render: (visibility: string) =>
+        visibility === "public" ? "✅ Да" : "❌ Нет",
+    },
+    {
+      title: "Действия",
+      key: "action",
+      render: (_: any, record: any) => {
+        return (
+          <Space>
+            {login && token && (
+              <UpdateRepo
+                login={login}
+                token={token}
+                repoName={record.name}
+                initialVisibility={record.visibility}
+              />
+            )}
+            {login && token && (
+              <DeleteRepoButton
+                login={login}
+                token={token}
+                repoName={record.name}
+              />
+            )}
+            {login && token && (
+              <RepoDetailsModal
+                login={login}
+                token={token}
+                repoName={record.name}
+              />
+            )}
+          </Space>
+        );
+      },
+    },
+  ];
+console.log(repositories)
+  if (status === "loading") return <p>Загрузка...</p>;
+  if (error) return <p>Ошибка: {error}</p>;
+
+  return <Table columns={columns} dataSource={repositories} rowKey="id" />;
+}
---
+++ src/pages/repos/ReposPage.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+import { Avatar, Layout } from "antd";
+
+import type { RootState } from "../../store/store";
+
+import { RepoTable } from "./RepoTable";
+import { CreateRepoButton } from "./CreateRepoButton";
+
+import "./style.css";
+
+const { Header, Content } = Layout;
+
+export function ReposPage() {
+  let navigate = useNavigate();
+  const { user, token } = useSelector((state: RootState) => state.user);
+
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  }, [token, navigate]);
+
+  return (
+    <main className="repos-container">
+      <Layout>
+        <Header className="header">
+          <CreateRepoButton login={user?.login} token={token} />
+          <div className="logo">
+            <div className="username">{user?.login || ""}</div>
+            <Avatar src={user?.avatar_url} />
+          </div>
+        </Header>
+        <Content className="content">
+          <RepoTable login={user?.login} token={token} />
+        </Content>
+      </Layout>
+    </main>
+  );
+}
